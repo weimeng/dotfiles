d96c4792fdb5eb7eaad1a58a53caedf96dc5e836
Wei-Meng Lee <weimeng@staticfusion.org>
2023-06-23 00:36:09 +0800

    docs: add comment-based help for Start-DotfilesGit

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -12,5 +12,20 @@ function Start-DotfilesGit
   $ArgumentList = @("--git-dir=$HOME\.dotfiles\", "--work-tree=$HOME") + ($Command -join ' ')
 
   Start-Process -NoNewWindow -Wait -FilePath "C:\Program Files\Git\cmd\git.exe" -ArgumentList $ArgumentList
+
+  <#
+    .SYNOPSIS
+    Wraps git with --git-dir set to .dotfiles and --work-tree set to $HOME.
+
+    .DESCRIPTION
+    Wraps git with --git-dir set to .dotfiles and --work-tree set to $HOME.
+
+    Arguments with quotes must only use double quotes (") and be escaped with a
+    backtick (`). See this GitHub issue for more details:
+    https://github.com/PowerShell/PowerShell/issues/5576#issuecomment-1127215848
+
+    .EXAMPLE
+    Start-DotfilesGit commit -m `"This is a commit message`"
+  #>
 }
 Set-Alias -Name dotfiles -Value Start-DotfilesGit
